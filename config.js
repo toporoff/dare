ab0941646880192013738bb6a9f23bf11c3ac9fb
Serghei T <toporoffs@gmail.com>
2018-08-20 21:16:49 +0300

    updated netAddress

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const config = module.exports;
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 config.express = {
-    port: process.env.EXPRESS_PORT || 5000,
+    port: process.env.EXPRESS_PORT || 3000,
     ip: '127.0.0.1'
 };
 
-config.netAddress = '165.227.175.170';
+config.netAddress = '199.192.19.119';
 config.defaultPrivateKey = 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr';
 config.winNodePublicKey = 'AN3S6qJY8xdEVWoxwUXuvbQk9jg8G2Y8gt';
 config.smartContractScriptHash = 'ac09adae571a06e12f1d54b6a0fe82ccf05efa8a'; // without 0x!
